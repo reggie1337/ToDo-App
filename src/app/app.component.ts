d157bc7426d7feaaec5353d5170ec0accdf4b652
reggie <reggie.skillman34@icloud.com>
2022-04-20 22:56:01 -0400

    actAdd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-
+import { Task } from './task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  activities: Task[] = [];
   form = this.fb.group({
-    activity: [''],
+    act: [''],
     cw: [''],
   });
+
   constructor(private fb: FormBuilder) {}
+
   newActivity() {
-    console.log(this.form.get('activity').value);
+    console.log(this.form.get('act').value);
     console.log(this.form.get('cw').value);
+    this.activities.push({
+      act: this.form.get('act').value,
+      cw: this.form.get('cw').value,
+    });
   }
 
   ngOnInit(): void {}
---
+++ src/app/task.ts
@@ -0,0 +1,4 @@
+export interface Task {
+  act: string;
+  cw?: Date;
+}
